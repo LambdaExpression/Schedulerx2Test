1e40fe4fe3cb5e7e1bf9c225b837ba80a0beb0a8
LambdaExpression <lambdaExpression@163.com>
2020-11-20 13:56:48 +0800

    commit

--- src/main/java/com/github/lambdaexpression/schedulerx/Test11JobProcessor.java
+++ src/main/java/com/github/lambdaexpression/schedulerx/Test11JobProcessor.java
@@ -1,27 +1,27 @@
-//package com.github.lambdaexpression.schedulerx;
-//
-//import com.alibaba.schedulerx.worker.domain.JobContext;
-//import com.alibaba.schedulerx.worker.processor.JavaProcessor;
-//import com.alibaba.schedulerx.worker.processor.ProcessResult;
-//import com.github.lambdaexpression.utils.ComputePiUtil;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//
-//import java.math.BigDecimal;
-//
-///**
-// * @author lin
-// * @date 2020/11/19
-// */
-//@Component
-//public class Test11JobProcessor extends JavaProcessor {
-//
-//    private Logger logger = LoggerFactory.getLogger(this.getClass());
-//
-//    @Override
-//    public ProcessResult process(JobContext context) throws Exception {
-//        TestSchedulerx.test(logger, this.getClass().getName());
-//        return new ProcessResult(true);
-//    }
-//}
+package com.github.lambdaexpression.schedulerx;
+
+import com.alibaba.schedulerx.worker.domain.JobContext;
+import com.alibaba.schedulerx.worker.processor.JavaProcessor;
+import com.alibaba.schedulerx.worker.processor.ProcessResult;
+import com.github.lambdaexpression.utils.ComputePiUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * @author lin
+ * @date 2020/11/19
+ */
+@Component
+public class Test11JobProcessor extends JavaProcessor {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public ProcessResult process(JobContext context) throws Exception {
+        TestSchedulerx.test(logger, this.getClass().getName());
+        return new ProcessResult(true);
+    }
+}
--- src/main/java/com/github/lambdaexpression/schedulerx/Test1JobProcessor.java
+++ src/main/java/com/github/lambdaexpression/schedulerx/Test1JobProcessor.java
@@ -1,24 +1,24 @@
-//package com.github.lambdaexpression.schedulerx;
-//
-//import com.alibaba.schedulerx.worker.domain.JobContext;
-//import com.alibaba.schedulerx.worker.processor.JavaProcessor;
-//import com.alibaba.schedulerx.worker.processor.ProcessResult;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @author lin
-// * @date 2020/11/19
-// */
-//@Component
-//public class Test1JobProcessor extends JavaProcessor {
-//
-//    private Logger logger = LoggerFactory.getLogger(this.getClass());
-//
-//    @Override
-//    public ProcessResult process(JobContext context) throws Exception {
-//        TestSchedulerx.test(logger, this.getClass().getName());
-//        return new ProcessResult(true);
-//    }
-//}
+package com.github.lambdaexpression.schedulerx;
+
+import com.alibaba.schedulerx.worker.domain.JobContext;
+import com.alibaba.schedulerx.worker.processor.JavaProcessor;
+import com.alibaba.schedulerx.worker.processor.ProcessResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lin
+ * @date 2020/11/19
+ */
+@Component
+public class Test1JobProcessor extends JavaProcessor {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public ProcessResult process(JobContext context) throws Exception {
+        TestSchedulerx.test(logger, this.getClass().getName());
+        return new ProcessResult(true);
+    }
+}
